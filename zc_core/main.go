4fb3a298a074fa0cce4952c0007be9e70526f326
Mark Essien <markessien@gmail.com>
2021-07-23 13:30:22 +0200

    Added various files as well as Gorilla mux

--- zc_core/main.go
+++ zc_core/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello Zuri Chat")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", VersionHandler)
+	http.Handle("/", r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func VersionHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Version 0.0001. Cat=%s\n", vars["category"])
 }
