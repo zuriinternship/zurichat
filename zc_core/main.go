f0d5aa56512bf9604d15220607d9cba78b267ae0
Mark Essien <markessien@gmail.com>
2021-07-25 11:08:29 +0200

    Fixed some bugs

--- zc_core/main.go
+++ zc_core/main.go
@@ -14,6 +14,8 @@ func Router() *mux.Router {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", VersionHandler)
+	r.HandleFunc("/testhtml", TestHtml)
+	r.HandleFunc("/testreact", TestReact)
 	r.HandleFunc("/data/write", data.WriteData)
 	r.HandleFunc("/data/read", data.ReadData)
 
@@ -23,7 +25,7 @@ func Router() *mux.Router {
 }
 
 func main() {
-	fmt.Println("Zuri Chat API running")
+	fmt.Println("Zuri Chat API running on 127.0.0.1:8000")
 
 	r := Router()
 
@@ -35,6 +37,16 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+func TestReact(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Zuri Chat API - Version 0.0001\n")
+}
+
+func TestHtml(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "<div><b>Hello</b> World</div>\n")
 }
 
 func VersionHandler(w http.ResponseWriter, r *http.Request) {
